ccf43945925832ac047a28fa6b011ef38aff31b2
www.brahim4 <www.brahim4@gmail.com>
2025-02-26 17:05:37 +0000

    validate 'search' and 'filters' query params

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $request->validate(['search' => 'string', 'filters' => 'string']);
+
         $articles = Article::with('source')
-            ->filter(request('filters'))
-            ->search(request('search'))
+            ->filter($request->get('filters'))
+            ->search($request->get('search'))
             ->orderByDesc('published_at')
             ->paginate()
             ->withQueryString();
--- app/Http/Controllers/ArticlePreferenceController.php
+++ app/Http/Controllers/ArticlePreferenceController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\ArticleResource;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticlePreferenceController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $preferences = request()->user()->preferences;
+        $request->validate(['search' => 'string', 'filters' => 'string']);
+
+        $preferences = $request->user()->preferences;
 
         $articles = Article::with('source')
             ->where(function ($query) use ($preferences) {
@@ -18,8 +21,8 @@ class ArticlePreferenceController extends Controller
                     ->orWhereIn('category_id', $preferences->preferred_categories ?? [])
                     ->orWhereIn('author', $preferences->preferred_authors ?? []);
             })
-            ->filter(request('filters'))
-            ->search(request('search'))
+            ->filter($request->get('filters'))
+            ->search($request->get('search'))
             ->orderByDesc('published_at')
             ->paginate()
             ->withQueryString();
